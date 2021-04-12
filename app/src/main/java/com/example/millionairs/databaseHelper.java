c146de044239cecceb9d60080f7bce15c6de3171
Nitzan Zaig <zaig@campus.technion.ac.il>
2021-04-12 16:09:46 +0300

    added beginning of Database

--- app/src/main/java/com/example/millionairs/databaseHelper.java
+++ app/src/main/java/com/example/millionairs/databaseHelper.java
@@ -1,9 +1,16 @@
 package com.example.millionairs;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class databaseHelper extends SQLiteOpenHelper {
+
+    public databaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
