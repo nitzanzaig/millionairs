3b62c159a27c163c3720b4f18115eb25bd30f76d
yuvalaro <yuvalaro@campus.technion.ac.il>
2021-04-29 19:00:46 +0300

    started login

--- app/src/main/java/com/example/millionairs/MainActivity.java
+++ app/src/main/java/com/example/millionairs/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.millionairs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void move(View view){
+        Intent intent = new Intent(getApplicationContext(), personaldetails.class);
+        startActivity(intent);
+    }
+
+    public void income(View view){
+        Intent intent = new Intent(getApplicationContext(), income.class);
+        startActivity(intent);
+    }
+    public void budget(View view){
+        Intent intent = new Intent(getApplicationContext(), budget.class);
+        startActivity(intent);
+    }
+    public void bot(View view){
+        Intent intent = new Intent(getApplicationContext(), bot.class);
+        startActivity(intent);
+    }
+
 }
