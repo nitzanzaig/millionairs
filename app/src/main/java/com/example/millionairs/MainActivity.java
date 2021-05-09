7cfa51731b0fe505b5fb3758e647b745269f76e0
yuvalaro <yuvalaro@campus.technion.ac.il>
2021-05-09 22:02:04 +0300

    started login

--- app/src/main/java/com/example/millionairs/MainActivity.java
+++ app/src/main/java/com/example/millionairs/MainActivity.java
@@ -6,29 +6,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    AnyChartView anyChartView;
+    String[] months = {"Jan", "Feb", "Mar"};
+    int[] earnings = {500, 600, 800};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anyChartView = findViewById(R.id.any_chart_view);
+        setupPiechart();
     }
 
+    public void setupPiechart() {
+        Pie pie = AnyChart.pie();
+        List<DataEntry> dataEntries = new ArrayList<>();
+        for (int i=0; i<months.length; i++){
+            dataEntries.add(new ValueDataEntry(months[i],earnings[i]));
+
+        }
+        pie.data(dataEntries);
+        anyChartView.setChart(pie);
+    }
+
+
     public void move(View view){
-        Intent intent = new Intent(getApplicationContext(), personaldetails.class);
+        //Intent intent = new Intent(getApplicationContext(), personaldetails.class);
+        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), logActivity.class);
         startActivity(intent);
     }
 
     public void income(View view){
-        Intent intent = new Intent(getApplicationContext(), income.class);
+        //Intent intent = new Intent(getApplicationContext(), income.class);
+        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), logActivity.class);
         startActivity(intent);
     }
     public void budget(View view){
-        Intent intent = new Intent(getApplicationContext(), budget.class);
+        //Intent intent = new Intent(getApplicationContext(), budget.class);
+        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), logActivity.class);
         startActivity(intent);
     }
     public void bot(View view){
-        Intent intent = new Intent(getApplicationContext(), bot.class);
+        //Intent intent = new Intent(getApplicationContext(), bot.class);
+        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), logActivity.class);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/millionairs/logActivity.java
+++ app/src/main/java/com/example/millionairs/logActivity.java
@@ -5,25 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-
-    Button button;
+public class logActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        button = findViewById(R.id.button2);
+        setContentView(R.layout.activity_log);
+        /*button = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
-        });
+        });*/
     }
-
+    public void move_to(View view){
+        //Intent intent = new Intent(getApplicationContext(), income.class);
+        //startActivity(intent);
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
 
 }
