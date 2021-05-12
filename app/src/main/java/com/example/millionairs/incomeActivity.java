4636e3d23b9737aa4cdbd079949912b57373a891
yuvalaro <yuvalaro@campus.technion.ac.il>
2021-05-12 17:25:14 +0300

    started login

--- app/src/main/java/com/example/millionairs/incomeActivity.java
+++ app/src/main/java/com/example/millionairs/incomeActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class income_expensesActivity extends AppCompatActivity {
+public class incomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_expenses);
+        setContentView(R.layout.activity_income);
     }
 }
